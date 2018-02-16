8e0aa88a3736d4de10fb9b7d01ec11bb7a72b322
Łukasz <ukasz321@gmail.com>
2018-02-16 18:23:32 +0100

    Added NintendoGame class

---
+++ src/test/NintendoGame.java
@@ -0,0 +1,25 @@
+package test;
+
+public class NintendoGame implements Product{
+
+	String name;
+	int price;
+	
+	public NintendoGame(String name, int price) {
+		this.name = name;
+		this.price = price;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+}
--- src/test/Shop.java
+++ src/test/Shop.java
@@ -9,12 +9,14 @@ public class Shop {
 		Product p1 = new PCGame("Witcher", 99);
 		Product p2 = new PSGame("Mario", 15);
 		Product p3 = new XboxGame("Tetris", 99);
+		Product p4 = new NintendoGame("Zelda", 49);
 		int sum = 0;
 		List <Product> cart = new ArrayList <Product> ();
 		cart.add(p1);
 		cart.add(p2);
-		cart.add(p3);	
-			
+		cart.add(p3);
+		cart.add(p4);
+		
 		for (Product x: cart){
 			sum+=x.getPrice();
 		}
